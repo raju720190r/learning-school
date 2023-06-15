59424cc8c4a115d6a56c38a7556110100fd0a0c3
raju720190r <raju720190r@gmail.com>
2023-06-15 22:14:24 +0600

    Use Admin And Axios Section

---
+++ src/Hooks/useAdmin.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query"
+import useAxiosSecure from "./useAxiosSecure"
+import useAuth from "./usAuth"
+
+
+
+const useAdmin = () => {
+    const { user ,  loading } = useAuth()
+    const [axiosSecure] = useAxiosSecure()
+    // use axiosSecure with react-query
+    const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
+        queryKey: ['isAdmin', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user?.email}`)
+            return res.data.admin
+        }
+    })
+    return [isAdmin, isAdminLoading]
+}
+
+export default useAdmin;
